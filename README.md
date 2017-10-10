e585db482e3739ad96fa8a8f5fcbe513e91d18fd
Arkadi Karetnikov <arkadik_ipex@yahoo.com>
2017-10-09 18:00:47 -0700

    Spesified URL for git pull in run-puppet

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Watch this space... coming soon!
+Watch this space... coming soon
+
+second modification 10/5/2017
--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /etc/puppetlabs/code/environments/production && git pull
+cd /etc/puppetlabs/code/environments/production && git pull https://github.com/karetnia/Puppet
 /opt/puppetlabs/bin/puppet apply manifests/
