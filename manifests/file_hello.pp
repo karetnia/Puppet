fd4e0dd2881650803c94d440853c87a269bba626
Arkadi Karetnikov <arkadik_ipex@yahoo.com>
2017-10-11 10:41:50 -0700

    Revmoved hello3

--- manifests/file_hello.pp
+++ manifests/file_hello.pp
@@ -7,12 +7,12 @@ file { '/tmp/hello2.txt':
  content => "hello, world2\n",
 }
 file { '/tmp/hello3.txt':
-<<<<<<< HEAD
+
  ensure => absent,
 
-=======
- ensure => file,
- content => "hello, world3\n",
->>>>>>> ef93b3aaea54ae216be21497e5ed43169c2c1c79
+
+
+
+
 }
  
